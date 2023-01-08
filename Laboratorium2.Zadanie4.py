4ad1641e85800caa77be097915df0d362e248cfe
OstapZapiatko <117644024+OstapZapiatko@users.noreply.github.com>
2023-01-08 19:35:46 +0200

    Add files via upload

---
+++ Laboratorium2.Zadanie4.py
@@ -0,0 +1,5 @@
+#Zadanie 4 (4.py). Napisz skrypt, która zamienia wprowadzoną literę na przeciwną (co do wielkości) i wypisuje
+#ją na ekranie.
+
+x = str(input('Wprowadźcie literę: '))
+print(x.swapcase())
